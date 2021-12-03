9888ab00e58df53d6dfea05aa2f17c8cf165d153
etudiant <alejeune.pro2@gmail.com>
2021-12-03 02:13:52 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import {Routes, Route, Link} from "react-router-dom"
 import Rescuer from "./templates/Rescuer"
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+        <Header/>
       <Link className="p-3" to="/">Hello</Link>
       <Link className="p-3" to="/fr">Bonjour</Link>
       <Routes>
