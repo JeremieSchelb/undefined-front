834e73a5bb1aa9236cb028b41c5ed2e7c95e1500
ANTZ ELOI <eloi.antz@etu.unistra.fr>
2021-12-03 03:26:38 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import './App.scss';
-import {Routes, Route, Link} from "react-router-dom"
-import Rescuer from "./templates/Rescuer"
+import "./App.scss";
+import { Routes, Route, Link } from "react-router-dom";
+import Rescuer from "./templates/Rescuer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-      <Link className="p-3" to="/">Hello</Link>
-      <Link className="p-3" to="/fr">Bonjour</Link>
-      <Routes>
-        <Route path="/fr" element={BonjourMonde()}/>
-        <Route path="/" element={HelloWorld()}/>
-        <Route path="/rescuer/:name" element={<Rescuer/>}/>
-      </Routes>
-        <Footer/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Link className="p-3" to="/">
+				Hello
+			</Link>
+			<Link className="p-3" to="/fr">
+				Bonjour
+			</Link>
+			<Routes>
+				<Route path="/fr" element={BonjourMonde()} />
+				<Route path="/" element={HelloWorld()} />
+				<Route path="/rescuer/:name" element={<Rescuer />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
-
-function HelloWorld(){
-  return <h1 className="App-link bg-light-blue">Hello world</h1>
+function HelloWorld() {
+	return <h1 className="App-link bg-light-blue">Hello world</h1>;
 }
 
-function BonjourMonde(){
-  return <h1 className="App-link bg-dark-blue">Bonjour monde</h1>
+function BonjourMonde() {
+	return <h1 className="App-link bg-dark-blue">Bonjour monde</h1>;
 }
 
 export default App;
