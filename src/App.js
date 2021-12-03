b66007227989c676fb10703924c8af572f6e6de2
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2021-12-03 03:37:09 +0100

    made responsive header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.scss';
-import {Routes, Route, Link} from "react-router-dom"
-import Rescuer from "./templates/Rescuer"
+import {Routes, Route} from "react-router-dom"
+import Rescuer from "./Templates/Rescuer"
 import Header from "./Components/Header"
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-      <Link className="p-3" to="/">Hello</Link>
-      <Link className="p-3" to="/fr">Bonjour</Link>
+      <Header/>
       <Routes>
         <Route path="/rescuer/:name" element={<Rescuer/>}/>
       </Routes>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,25 @@
+import { useEffect } from "react";
 import {Link} from "react-router-dom";
+import useToggle from "../Hooks/useToggle"
 
 const links = ["accueil", "sauveteurs", "sorties en mer", "stations", "naufragés", "bateaux", "glossaire"]
 const Header = (props) => {
+
+    const [open, toggleOpen] = useToggle(false)
+
     return (
-        <header className={'max-w-screen-xl flex py-5 items-center m-auto justify-between'}>
-            <nav className={'flex items-center'}>
-            <img src="img/logo.png" alt=""/>
-                <ul className={'flex'}>
-                    <ul className={'flex'}>
-                        {links.map(link => <li className={'px-5 hover:bg-light-blue hover:text-light-grey'}><Link to={link}>{link}</Link></li>)}
-                    </ul>
+        <header className="header">
+            <img onClick={() => toggleOpen(true)} className="xl:hidden cursor-pointer" src="img/burger.svg" alt=""/>
+            <div><img className="hidden xl:inline-block" src="img/logo.png" alt=""/></div>
+            <nav className={`fixed inset-0 bg-light-grey flex flex-col gap-2 p-5 transition-transform xl:static ${open ? "" : "transform -translate-x-full xl:-translate-x-0"}`}>
+                <img style={{maxWidth : '25px'}} onClick={() => toggleOpen(false)} className="xl:hidden cursor-pointer" src="img/burger-off.svg" alt=""/>
+                <ul className={'flex flex-col xl:flex-row'}>
+                    {links.map(link => <li key={link} className={'p-3 up-first transition rounded-md hover:bg-light-blue hover:text-light-grey'}><Link to={link}>{link}</Link></li>)}
                 </ul>
             </nav>
-            <img src="img/search.svg" alt=""/>
+            <img onClick={() => toggleOpen()} className="xl:hidden mx-auto" src="img/logo.png" alt=""/>
+            <img src="img/search.svg" className="cursor-pointer ml-auto" alt=""/>
         </header>
     )
-
 }
 export default Header;
