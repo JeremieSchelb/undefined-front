f7c2971780b3e529e4d4c0eee0c68f9448fe1ddf
QuentiumYT <quentin.lienhardt@gmail.com>
2022-02-10 00:32:32 +0100

    Remove log + lint

--- src/Components/ArticleTemplates.jsx
+++ src/Components/ArticleTemplates.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function ArticleTemplates(props) {
 
@@ -6,62 +6,60 @@ export default function ArticleTemplates(props) {
     const [template, setTemplate] = useState('');
 
     useEffect(() => {
-        console.log(item);
-
         switch (props.type) {
             case 'sauveteurs':
                 setTemplate(
                     <li className="shadow-md p-4 my-4" >
-                    <div className="flex justify-between">
-                        <h2 className="font-bold">{item.lastname} {item.firstname}</h2>
-                        <span>{item.created_at}</span>
-                    </div>
-                    <p>{item.biography}</p>
-                </li>);
+                        <div className="flex justify-between">
+                            <h2 className="font-bold">{item.lastname} {item.firstname}</h2>
+                            <span>{item.created_at}</span>
+                        </div>
+                        <p>{item.biography}</p>
+                    </li>);
                 break;
             case 'sorties en mer':
                 setTemplate(
                     <li className="shadow-md p-4 my-4" >
-                    <div className="flex justify-between">
-                        <h2 className="font-bold">{item.location}</h2>
-                        <span>{item.created_at}</span>
-                    </div>
-                    <p>{item.report}</p>
-                </li>);
+                        <div className="flex justify-between">
+                            <h2 className="font-bold">{item.location}</h2>
+                            <span>{item.created_at}</span>
+                        </div>
+                        <p>{item.report}</p>
+                    </li>);
                 break;
             case 'stations':
                 setTemplate(
                     <li className="shadow-md p-4 my-4" >
-                    <div className="flex justify-between">
-                        <h2 className="font-bold">{item.name}</h2>
-                        <span>{item.created_at}</span>
-                    </div>
-                    <p>{item.history}</p>
-                </li>);
+                        <div className="flex justify-between">
+                            <h2 className="font-bold">{item.name}</h2>
+                            <span>{item.created_at}</span>
+                        </div>
+                        <p>{item.history}</p>
+                    </li>);
                 break;
             case 'naufrages':
                 setTemplate(
                     <li className="shadow-md p-4 my-4" >
-                    <div className="flex justify-between">
-                        <h2 className="font-bold">{item.lastname} {item.firstname}</h2>
-                        <span>{item.created_at}</span>
-                    </div>
-                </li>);
+                        <div className="flex justify-between">
+                            <h2 className="font-bold">{item.lastname} {item.firstname}</h2>
+                            <span>{item.created_at}</span>
+                        </div>
+                    </li>);
                 break;
-            case 'bateaux' :
+            case 'bateaux':
                 setTemplate(
-                <li className="shadow-md p-4 my-4" >
-                    <div className="flex justify-between">
-                        <h2 className="font-bold">{item.name}</h2>
-                        <span>{item.created_at}</span>
-                    </div>
-                    <p>{item.description}</p>
-                </li>);
+                    <li className="shadow-md p-4 my-4" >
+                        <div className="flex justify-between">
+                            <h2 className="font-bold">{item.name}</h2>
+                            <span>{item.created_at}</span>
+                        </div>
+                        <p>{item.description}</p>
+                    </li>);
                 break;
         }
-    }, [])
+    }, []);
 
     return (
         template
-    )
+    );
 }
